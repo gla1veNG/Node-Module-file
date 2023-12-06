93d8491d8e0fab0eef9be554279cefa8e92bd12f
TR <2993078435@qq.com>
2023-12-06 20:13:10 +0800

    发布到了 npm ,可以通过 yarn global add nodes-todo 进行下载

--- cli.js
+++ cli.js
@@ -1,5 +1,6 @@
-const program= require('commander');
-const api  =require('./index.js');
+#!/usr/bin/env node
+const program = require('commander');
+const api = require('./index.js');
 
 program
   .option('-x, --xxx', 'what ths x');
@@ -7,19 +8,19 @@ program
   .command('add')
   .description('add a task')
   .action((...args) => {
-    const words = args.slice(0,-1).join(' ');
-    api.add(words).then(()=>{console.log('添加成功')},()=>{console.log('添加失败')});
+    const words = args.slice(0, -1).join(' ');
+    api.add(words).then(() => { console.log('添加成功') }, () => { console.log('添加失败') });
   });
 program
   .command('clear')
   .description('clear all task')
   .action(() => {
-    api.clear().then(()=>{console.log('清除完毕')},()=>{console.log('清除失败')});
+    api.clear().then(() => { console.log('清除完毕') }, () => { console.log('清除失败') });
   });
 
 program.parse(process.argv);
 
-if(process.argv.length === 2){
-    //说明用户直接运行 node cli.js
-    api.showAll();
+if (process.argv.length === 2) {
+  //说明用户直接运行 node cli.js
+  api.showAll();
 }
